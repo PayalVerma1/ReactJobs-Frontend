f550665ec26bcd7f75404e751a251c0b3e965997
PayalVerma1 <payalvermaseema@gmail.com>
2025-02-13 09:40:53 +0530

    All Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 
 // import Viewjobs from './components/Viewjobs';
 import Mainlayout from './layouts/Mainlayout';
 import Homepage from './Pages/Homepage';
+import Jobspage from './Pages/Jobspage';
+import Addjobs from './Pages/Addjobs';
 const router=createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Mainlayout/>}>
     <Route index element={<Homepage/>}/>
-    
+    <Route path='/jobs' element={<Jobspage/>}/>
+    <Route path='/addjobs' element={<Addjobs/>}/>
   </Route>,
 ));
 const App = () => {
